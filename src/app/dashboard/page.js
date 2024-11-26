99886307c156b80ba3a7cfa583cc55032e087dc0
madoka <mnogaki@my.bcit.ca>
2024-11-25 22:46:05 -0800

    add back link

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,15 +7,25 @@ export default function MedicationDetails() {
         <div>
             <Header />
             <main className={styles.main}>
-                <h2>Medication Name: Insulin Lispro</h2>
-                <p>Drug identification number: ####</p>
-                <p>Brand Name: Lorem</p>
-                <p>Company Name: Lorem</p>
-
+                <div className={styles.links}>
+                    <a href='/'>Search</a>
+                    <p>/</p>
+                    <p className={styles.currentPage}>Insulin Lispro</p>
+                </div>
+                <div className={styles.medName}>
+                    <h2>Insulin Lispro</h2>
+                    <p>Drug identification number: ####</p>
+                    <p>Brand Name: Lorem</p>
+                    <p>Company Name: Lorem</p>
+                </div>
                 <div className={styles.outerBentoContainer}>
                     <section>
                         <h3>Drug Info</h3>
                         <ul>
+                            <li>Dosage Form</li>
+                            <li>Packaging</li>
+                            <li>Intake Form:</li>
+                            <li>Product Status</li>
                             <li>Dosage Form</li>
                             <li>Packaging</li>
                             <li>Intake Form:</li>
